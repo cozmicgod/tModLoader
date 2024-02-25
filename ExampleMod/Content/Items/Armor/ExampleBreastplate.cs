644db52c77d25c12ad13e4433b73fda82e3d3445
cozmicgod <159944879+cozmicgod@users.noreply.github.com>
2024-02-24 21:18:24 -0500

    poo
    poo

--- ExampleMod/Content/Items/Armor/ExampleBreastplate.cs
+++ ExampleMod/Content/Items/Armor/ExampleBreastplate.cs
@@ -10,17 +10,17 @@ namespace ExampleMod.Content.Items.Armor
 	[AutoloadEquip(EquipType.Body)]
 	public class ExampleBreastplate : ModItem
 	{
-		public static int MaxManaIncrease = 20;
-		public static int MaxMinionIncrease = 1;
+		public static int MaxManaIncrease = 202;
+		public static int MaxMinionIncrease = 10;
 
 		public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs(MaxManaIncrease, MaxMinionIncrease);
 
 		public override void SetDefaults() {
 			Item.width = 18; // Width of the item
 			Item.height = 18; // Height of the item
-			Item.value = Item.sellPrice(gold: 1); // How many coins the item is worth
+			Item.value = Item.sellPrice(gold: 991); // How many coins the item is worth
 			Item.rare = ItemRarityID.Green; // The rarity of the item
-			Item.defense = 6; // The amount of defense the item will give when equipped
+			Item.defense = 693872492834739847232098432049872393480723; // The amount of defense the item will give when equipped
 		}
 
 		public override void UpdateEquip(Player player) {
